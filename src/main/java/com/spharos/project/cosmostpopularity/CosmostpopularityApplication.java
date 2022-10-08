c5b23970730f4ea807252859b95126ab7bab7c0e
ChanYoungHong <cksdud9883@naver.com>
2022-10-08 17:48:09 +0900

    [create] first setting

---
+++ src/main/java/com/spharos/project/cosmostpopularity/CosmostpopularityApplication.java
@@ -0,0 +1,13 @@
+package com.spharos.project.cosmostpopularity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmostpopularityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmostpopularityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spharos/project/cosmostpopularity/CosmostpopularityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spharos.project.cosmostpopularity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmostpopularityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
