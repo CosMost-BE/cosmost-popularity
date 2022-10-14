33e8ecdf0cf58cbc2c3b66739f840c492ecb1085
ChanYoungHong <cksdud9883@naver.com>
2022-10-15 03:26:16 +0900

    [update] add createCourseReviewThumbsup method

--- src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupServiceImpl.java
+++ src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupServiceImpl.java
@@ -1,13 +1,29 @@
 package com.spharos.project.cosmostpopularity.service;
 
+import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseReviewThumbsupEntity;
+import com.spharos.project.cosmostpopularity.infrastructure.repository.CourseReviewThumbsupRepository;
 import com.spharos.project.cosmostpopularity.requestbody.CreateCourseReviewThumbsupRequest;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@ToString
 public class CourseReviewThumbsupServiceImpl implements CourseReviewThumbsupService{
 
-    @Override
-    public void createCourseReviewThumbsup(CreateCourseReviewThumbsupRequest createCourseReviewThumbsupRequest) {
+    private final CourseReviewThumbsupRepository courseReviewThumbsupRepository;
 
+    @Autowired
+    public CourseReviewThumbsupServiceImpl(CourseReviewThumbsupRepository courseReviewThumbsupRepository) {
+        this.courseReviewThumbsupRepository = courseReviewThumbsupRepository;
     }
+
+    @Override
+    public void createCourseReviewThumbsup(CreateCourseReviewThumbsupRequest request) {
+        CourseReviewThumbsupEntity courseReviewThumbsupEntity = dtoToEntity(request);
+        courseReviewThumbsupRepository.save(courseReviewThumbsupEntity);
+    }
+
 }
