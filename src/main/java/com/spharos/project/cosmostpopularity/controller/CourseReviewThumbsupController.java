140d0f7cae1f62cbd1c5daa53bfc08185573800c
ChanYoungHong <cksdud9883@naver.com>
2022-10-15 03:25:07 +0900

    [update] add PostMapping for createCourseReviewThumbsup

--- src/main/java/com/spharos/project/cosmostpopularity/controller/CourseReviewThumbsupController.java
+++ src/main/java/com/spharos/project/cosmostpopularity/controller/CourseReviewThumbsupController.java
@@ -1,5 +1,11 @@
 package com.spharos.project.cosmostpopularity.controller;
 
+import com.spharos.project.cosmostpopularity.requestbody.CreateCourseReviewThumbsupRequest;
+import com.spharos.project.cosmostpopularity.service.CourseReviewThumbsupService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/v1/popularities")
+@Slf4j
 public class CourseReviewThumbsupController {
 
-    @PostMapping("")
-    public ResponseEntity<?> createCourseReviewThumbsup(){
+    private final CourseReviewThumbsupService courseReviewThumbsupService;
 
-        return null;
+    @Autowired
+    public CourseReviewThumbsupController(CourseReviewThumbsupService courseReviewThumbsupService) {
+        this.courseReviewThumbsupService = courseReviewThumbsupService;
+    }
+
+    //코스리뷰 좋아요 등록
+    @PostMapping("")
+    public ResponseEntity<String> createCourseReviewThumbsup(CreateCourseReviewThumbsupRequest request){
+        log.info(String.valueOf(request.getAuthId()));
+        log.info(String.valueOf(request.getCourseId()));
+        courseReviewThumbsupService.createCourseReviewThumbsup(request);
+        return ResponseEntity.ok("코스리뷰 좋아요 성공!!");
+
     }
 
 }
