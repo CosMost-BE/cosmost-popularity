b795f6946a3d58404ee95641dca3887f3de11cd0
chanyoung Hong <cksdud9883@naver.com>
2022-10-14 22:47:58 +0900

    [update] add createCourseReviewThumbsup method

--- src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupServiceImpl.java
+++ src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupServiceImpl.java
@@ -1,8 +1,13 @@
 package com.spharos.project.cosmostpopularity.service;
 
+import com.spharos.project.cosmostpopularity.requestbody.CreateCourseReviewThumbsupRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseReviewThumbsupServiceImpl implements CourseReviewThumbsupService{
 
+    @Override
+    public void createCourseReviewThumbsup(CreateCourseReviewThumbsupRequest createCourseReviewThumbsupRequest) {
+
+    }
 }
