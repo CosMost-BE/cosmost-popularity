0cc34f71580b1753ee5377f374028e712b51a541
chanyoung Hong <cksdud9883@naver.com>
2022-10-14 22:47:07 +0900

    [update] add postmapping in Controller

--- src/main/java/com/spharos/project/cosmostpopularity/controller/CourseReviewThumbsupController.java
+++ src/main/java/com/spharos/project/cosmostpopularity/controller/CourseReviewThumbsupController.java
@@ -1,6 +1,8 @@
 package com.spharos.project.cosmostpopularity.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/popularities")
 public class CourseReviewThumbsupController {
 
+    @PostMapping("")
+    public ResponseEntity<?> createCourseReviewThumbsup(){
+
+        return null;
+    }
 
 }
