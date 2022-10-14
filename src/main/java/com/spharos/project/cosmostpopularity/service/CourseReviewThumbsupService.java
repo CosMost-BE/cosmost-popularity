2e5cd75e456468355f06f0184e637c990c313162
ChanYoungHong <cksdud9883@naver.com>
2022-10-15 03:25:51 +0900

    [update] add createCourseReviewThumbsup method

--- src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupService.java
+++ src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupService.java
@@ -1,11 +1,21 @@
 package com.spharos.project.cosmostpopularity.service;
 
 
+import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseReviewThumbsupEntity;
 import com.spharos.project.cosmostpopularity.requestbody.CreateCourseReviewThumbsupRequest;
 
 public interface CourseReviewThumbsupService {
 
-    void createCourseReviewThumbsup(CreateCourseReviewThumbsupRequest createCourseReviewThumbsupRequest);
+    void createCourseReviewThumbsup(CreateCourseReviewThumbsupRequest request);
 
+    default CourseReviewThumbsupEntity dtoToEntity(CreateCourseReviewThumbsupRequest request) {
+
+        CourseReviewThumbsupEntity courseReviewThumbsupEntity = CourseReviewThumbsupEntity.builder()
+                .courseId(request.getCourseId())
+                .authId(request.getAuthId())
+                .build();
+
+        return courseReviewThumbsupEntity;
+    }
 
 }
