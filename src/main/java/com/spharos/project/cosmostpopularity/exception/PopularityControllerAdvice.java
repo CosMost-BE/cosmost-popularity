fc887dd7eec40fafbbb6a0b5e6c0880b7d4f702a
ChanYoungHong <cksdud9883@naver.com>
2022-10-16 01:39:40 +0900

    [update] rename class

--- src/main/java/com/spharos/project/cosmostpopularity/exception/PopularityControllerAdvice.java
+++ src/main/java/com/spharos/project/cosmostpopularity/exception/PopularityControllerAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class CourseReviewThumbsupControllerAdvice {
+public class PopularityControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public String processValidationError(MethodArgumentNotValidException exception) {
