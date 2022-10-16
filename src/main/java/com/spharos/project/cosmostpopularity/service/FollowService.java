2d6aff72dec0bd18f838b1eec394893858874f3b
ChanYoungHong <cksdud9883@naver.com>
2022-10-16 20:41:17 +0900

    [update] add readMyFollwers in FollowService

--- src/main/java/com/spharos/project/cosmostpopularity/service/FollowService.java
+++ src/main/java/com/spharos/project/cosmostpopularity/service/FollowService.java
@@ -1,12 +1,18 @@
 package com.spharos.project.cosmostpopularity.service;
 
 import com.spharos.project.cosmostpopularity.infrastructure.entity.FollowEntity;
+import com.spharos.project.cosmostpopularity.model.Follow;
 import com.spharos.project.cosmostpopularity.requestbody.CreatePopularitiesRequest;
+
+import java.util.List;
 
 public interface FollowService {
 
     void createFollow(CreatePopularitiesRequest request);
     void deleteFollow(Long id);
+    List<Follow> readMyFollwers();
+
+
 
     default FollowEntity followdtoToEntity(CreatePopularitiesRequest request) {
         FollowEntity followEntity = FollowEntity.builder()
