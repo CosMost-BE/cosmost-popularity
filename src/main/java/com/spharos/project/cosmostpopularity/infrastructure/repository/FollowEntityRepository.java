5906c97d436c5bb280e71e97a837f2d9460a641c
ChanYoungHong <cksdud9883@naver.com>
2022-10-16 20:40:02 +0900

    [update] add findAllByAuthId in FollowEntityRepository

--- src/main/java/com/spharos/project/cosmostpopularity/infrastructure/repository/FollowEntityRepository.java
+++ src/main/java/com/spharos/project/cosmostpopularity/infrastructure/repository/FollowEntityRepository.java
@@ -1,10 +1,15 @@
 package com.spharos.project.cosmostpopularity.infrastructure.repository;
 
 import com.spharos.project.cosmostpopularity.infrastructure.entity.FollowEntity;
+import com.spharos.project.cosmostpopularity.model.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FollowEntityRepository extends JpaRepository<FollowEntity, Long> {
+
+    List<FollowEntity> findAllByAuthId(Long id);
 
 }
