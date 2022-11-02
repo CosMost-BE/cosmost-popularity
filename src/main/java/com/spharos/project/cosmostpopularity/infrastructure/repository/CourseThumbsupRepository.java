dc8337494d6821217832cf290a3167b0db071c42
chanyoung Hong <cksdud9883@naver.com>
2022-11-02 12:12:45 +0900

    [update] add findByAuthId method

--- src/main/java/com/spharos/project/cosmostpopularity/infrastructure/repository/CourseThumbsupRepository.java
+++ src/main/java/com/spharos/project/cosmostpopularity/infrastructure/repository/CourseThumbsupRepository.java
@@ -4,6 +4,10 @@ import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseThumbsu
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseThumbsupRepository extends JpaRepository<CourseThumbsupEntity, Long> {
+
+    List<CourseThumbsupEntity> findByAuthId(Long id);
 }
