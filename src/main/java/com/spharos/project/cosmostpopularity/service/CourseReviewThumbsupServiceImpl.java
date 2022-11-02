24f15cc1751dba94bdea34e7c2feedc6cd765be9
chanyoung Hong <cksdud9883@naver.com>
2022-11-02 12:12:14 +0900

    [update] add Authorization code

--- src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupServiceImpl.java
+++ src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupServiceImpl.java
@@ -5,15 +5,25 @@ import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseReviewT
 import com.spharos.project.cosmostpopularity.infrastructure.repository.CourseReviewThumbsupEntityRepository;
 import com.spharos.project.cosmostpopularity.requestbody.CreatePopularitiesRequest;
 import java.util.Optional;
+
+import io.jsonwebtoken.Jwts;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Service
 @ToString
 public class CourseReviewThumbsupServiceImpl implements CourseReviewThumbsupService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     private final CourseReviewThumbsupEntityRepository courseReviewThumbsupRepository;
 
@@ -24,8 +34,15 @@ public class CourseReviewThumbsupServiceImpl implements CourseReviewThumbsupServ
     }
 
     @Override
-    public void createCourseReviewThumbsup(CreatePopularitiesRequest request) {
-        CourseReviewThumbsupEntity courseReviewThumbsupEntity = reviewThumbsupdtoToEntity(request);
+    public void createCourseReviewThumbsup(CreatePopularitiesRequest createPopularitiesRequest) {
+
+        HttpServletRequest request = ((ServletRequestAttributes)
+                RequestContextHolder.currentRequestAttributes()).getRequest();
+        String token = request.getHeader("Authorization");
+        Long id = Long.parseLong(Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject());
+
+
+        CourseReviewThumbsupEntity courseReviewThumbsupEntity = reviewThumbsupdtoToEntity(createPopularitiesRequest);
         courseReviewThumbsupRepository.save(courseReviewThumbsupEntity);
     }
 
