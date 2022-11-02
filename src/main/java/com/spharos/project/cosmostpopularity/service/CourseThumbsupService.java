19041fc05eb373f110af7d3c660fd1a47373e9f4
chanyoung Hong <cksdud9883@naver.com>
2022-11-02 12:13:10 +0900

    [update] add readThumbsupByMe method

--- src/main/java/com/spharos/project/cosmostpopularity/service/CourseThumbsupService.java
+++ src/main/java/com/spharos/project/cosmostpopularity/service/CourseThumbsupService.java
@@ -1,14 +1,17 @@
 package com.spharos.project.cosmostpopularity.service;
 
 
-import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseReviewThumbsupEntity;
 import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseThumbsupEntity;
 import com.spharos.project.cosmostpopularity.requestbody.CreatePopularitiesRequest;
+import com.spharos.project.cosmostpopularity.view.CourseThumbsupView;
+
+import java.util.List;
 
 public interface CourseThumbsupService {
 
     void createCourseThumbsup(CreatePopularitiesRequest request);
     void deleteCourseThumbsup(Long id);
+    List<CourseThumbsupView> readThumbsupByMe(Long id);
 
     default CourseThumbsupEntity courseThumbsupdtoToEntity(CreatePopularitiesRequest request) {
 
