88652e5196c8fcda8210a46bdfea7336f83aac65
chanyoung Hong <cksdud9883@naver.com>
2022-11-03 02:14:02 +0900

    [update] add findByAuthIdAndCourseReviewId method

--- src/main/java/com/spharos/project/cosmostpopularity/infrastructure/repository/CourseReviewThumbsupEntityRepository.java
+++ src/main/java/com/spharos/project/cosmostpopularity/infrastructure/repository/CourseReviewThumbsupEntityRepository.java
@@ -1,10 +1,14 @@
 package com.spharos.project.cosmostpopularity.infrastructure.repository;
 
 import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseReviewThumbsupEntity;
+import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseThumbsupEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourseReviewThumbsupEntityRepository extends JpaRepository<CourseReviewThumbsupEntity, Long> {
 
+    List<CourseReviewThumbsupEntity> findByAuthIdAndCourseReviewId(Long authId, Long courseReviewId);
 }
