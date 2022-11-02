d0ef722f60c0b60176f4566b20ca41355c27f564
chanyoung Hong <cksdud9883@naver.com>
2022-11-02 15:10:44 +0900

    [create] add CourseThumbsupCountView class

---
+++ src/main/java/com/spharos/project/cosmostpopularity/view/CourseThumbsupCountView.java
@@ -0,0 +1,16 @@
+package com.spharos.project.cosmostpopularity.view;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CourseThumbsupCountView {
+
+    private Long courseThumbsCnt;
+
+}
