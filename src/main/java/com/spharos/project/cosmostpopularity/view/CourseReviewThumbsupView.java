ad0644fc1bcab6977aeb498e55db5b1e4c9a8a6b
chanyoung Hong <cksdud9883@naver.com>
2022-11-03 20:18:01 +0900

    [update] add Constructor

--- src/main/java/com/spharos/project/cosmostpopularity/view/CourseReviewThumbsupView.java
+++ src/main/java/com/spharos/project/cosmostpopularity/view/CourseReviewThumbsupView.java
@@ -1,6 +1,8 @@
 package com.spharos.project.cosmostpopularity.view;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseReviewThumbsupEntity;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,10 +10,16 @@ import lombok.ToString;
 @Getter
 @ToString
 @Builder
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CourseReviewThumbsupView {
 
     private Long authId;
     private Long courseReviewId;
+
+    public CourseReviewThumbsupView(CourseReviewThumbsupEntity entity) {
+        this.authId = entity.getAuthId();
+        this.courseReviewId = entity.getCourseReviewId();
+    }
 
 }
