e9dda727a5f154dfcccf44c77cbc805b9070072b
chanyoung Hong <cksdud9883@naver.com>
2022-11-03 20:16:48 +0900

    [update] add method

--- src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupService.java
+++ src/main/java/com/spharos/project/cosmostpopularity/service/CourseReviewThumbsupService.java
@@ -1,15 +1,17 @@
 package com.spharos.project.cosmostpopularity.service;
 
 
-import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseReviewThumbsupEntity;
 import com.spharos.project.cosmostpopularity.model.CourseReviewThumbsup;
 import com.spharos.project.cosmostpopularity.requestbody.CreatePopularitiesRequest;
+import com.spharos.project.cosmostpopularity.view.CourseReviewThumbsupView;
+
+import java.util.List;
 
 public interface CourseReviewThumbsupService {
 
     CourseReviewThumbsup createCourseReviewThumbsup(CreatePopularitiesRequest request);
     void deleteCourseReviewThumbsup(Long id);
+    List<CourseReviewThumbsupView> readCourseReviewThumbsupByMe(Long id);
 
-
-
+    CourseReviewThumbsup readCourseReviewThumbsupCount(Long id);
 }
