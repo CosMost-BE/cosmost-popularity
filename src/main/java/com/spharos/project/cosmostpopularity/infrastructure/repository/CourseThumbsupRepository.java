3025c75b84c1087398667f6311cc4b4da6270eee
chanyoung Hong <cksdud9883@naver.com>
2022-11-04 17:16:16 +0900

    [update] change type for findAllByAuthId

--- src/main/java/com/spharos/project/cosmostpopularity/infrastructure/repository/CourseThumbsupRepository.java
+++ src/main/java/com/spharos/project/cosmostpopularity/infrastructure/repository/CourseThumbsupRepository.java
@@ -1,8 +1,8 @@
 package com.spharos.project.cosmostpopularity.infrastructure.repository;
 
 import com.spharos.project.cosmostpopularity.infrastructure.entity.CourseThumbsupEntity;
-import com.spharos.project.cosmostpopularity.view.CourseThumbsupCountView;
-import com.spharos.project.cosmostpopularity.view.CourseThumbsupView;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +13,5 @@ public interface CourseThumbsupRepository extends JpaRepository<CourseThumbsupEn
 
     List<CourseThumbsupEntity> findByAuthIdAndCourseId(Long authId, Long courseId);
     Long countByCourseId(Long id);
-    List<CourseThumbsupEntity> findAllByAuthId(Long id);
+    Slice<CourseThumbsupEntity> findAllByAuthId(Long id, Pageable pageable);
 }
